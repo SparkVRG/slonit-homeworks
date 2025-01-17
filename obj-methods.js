311a9b8b712520c0e5484fe3b8b0942fc9b2d3e0
SparkVRG <sparkvrg@yandex.ru>
2025-01-17 17:36:47 +0300

    Добавлен скрипт для задания по повторению объектов

---
+++ obj-methods.js
@@ -0,0 +1,42 @@
+let exampleObj = {
+    name: 'Vasya',
+    friends: 5,
+    likes: 19,
+    projects: 7,
+
+    getSumNumericFields() {
+        let result = 0;
+
+        for (let key in this) {
+            if (!Number.isNaN(Number(this[key]))) {
+                result += this[key];
+            }
+        }
+
+        return result;
+    },
+
+    getArrNumericFieldsSortedInDescendingOrder() {
+        let arrNumericFields = [];
+        let result = [];
+
+        for (let key in this) {
+            if (!Number.isNaN(Number(this[key]))) {
+                arrNumericFields.push([key, this[key]]);
+            }
+        }
+
+        arrNumericFields.sort((a, b) => {
+            return (b[1] - a[1]);
+        });
+
+        for (let elem of arrNumericFields) {
+            result.push(elem[0]);
+        }
+
+        return result;
+    }
+};
+
+console.log(exampleObj.getSumNumericFields());
+console.log(exampleObj.getArrNumericFieldsSortedInDescendingOrder());
